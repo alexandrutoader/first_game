58e6c86e251cab0f83232ff1ddc2fa8ade6e69fa
alexandrutoader <toader.alexandru02@gmail.com>
2017-02-27 17:12:44 +0200

    Connect to database

---
+++ tests/form/scor.sql
@@ -0,0 +1,26 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.16-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table test.scor
+CREATE TABLE IF NOT EXISTS `scor` (
+  `Player` tinyint(50) unsigned DEFAULT NULL,
+  `Computer` tinyint(50) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table test.scor: ~0 rows (approximately)
+/*!40000 ALTER TABLE `scor` DISABLE KEYS */;
+INSERT IGNORE INTO `scor` (`Player`, `Computer`) VALUES
+	(0, 0);
+/*!40000 ALTER TABLE `scor` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
